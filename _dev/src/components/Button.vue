4d45280a9fddf29acd63352e6f9c98a63845a6ff
Yohan Barilleau <barilyoh@gmail.com>
2020-07-31 15:43:11 +0200

    Setup Storybook install + example with Button.vue

---
+++ _dev/src/components/Button.vue
@@ -0,0 +1,21 @@
+<template>
+  <button @click="handleClick">
+    <router-link v-if="url && url.indexOf('http') == -1" :title="label" :to="url">{{ label }}</router-link>
+    <a v-else-if="url" :href="url" target="_blank" :title="label" rel="noopener">{{ label }}</a>
+    <span v-else-if="label">{{ label }}</span>
+    <slot v-else/>
+  </button>
+</template>
+<script>
+export default {
+  props: {
+    label: String,
+    url: String
+  },
+  methods: {
+    handleClick(e) {
+      this.$emit('click', e);
+    }
+  }
+}
+</script>
